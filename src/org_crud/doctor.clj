ff33f0cd0ae3607c3e6abb845b7746802686020b
Russell Matney <russell.matney@gmail.com>
2021-02-14 19:49:56 -0500

    feat: doctor cli command prints duplciate uuids
    I recently broke parts of my org setup by refactoring some folders
    together and combining things with roam - roam now does me the service
    of throwing a huge error when trying to process some of my files,
    because there are a large number of duplicated uuids in there. That part
    is my fault, and it's an annoying problem to have, especially when it
    means a big buffer printing a huge group of uuids and saying i thinks
    that at least one is a duplicate.
    
    This commit introduces a cli command 'doctor', whos first task is
    analyzing your org-directory for problems, specifically duplicate uuids.
    Duplicates are logged. If this is common or there are quite alot, maybe
    we'll add some interactive support for updating the particular items,
    'Carve' style.

--- src/org_crud/doctor.clj
+++ src/org_crud/doctor.clj
@@ -2,21 +2,9 @@
   (:require [org-crud.fs :as fs]
             [org-crud.core :as core]))
 
-(defn ->report-data [f]
-  ;; NOTE this is only level-one totals/averages, not for the whole nested item
-  ;; TODO improve reporting here - should not have expectations around structure
-  (let [name (fs/base-name f)
-        items (->> (core/path->nested-item f) :org/items)
-        total (count items)
-        word-count (when (seq items) (reduce + 0 (map :org/word-count items)))
-        average-word-count (when (seq items) (int (/ word-count total)))]
-    {:report/name name
-     :report/total total
-     :report/word-count word-count
-     :report/average-word-count average-word-count}))
-
-(comment
-  (->report-data "/home/russ/todo/prompts.org"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; org helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn org-files
   "Fetches org-files.
@@ -37,26 +25,73 @@
   (count (org-files)))
 
 (defn file->ids [f]
-  ;; TODO walk the tree, collecting uniques and duplicates
-  (set (:org/id f)))
+  (->> f
+       core/path->flattened-items
+       (map :org/id)))
+
+(comment
+  (file->ids "/home/russ/todo/prompts.org")
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; duplicate ids
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn single-file-uuid-duplicate [file]
+  (let [items (core/path->flattened-items file)]
+    (loop [remaining-items items
+           checked-ids     #{}]
+      (let [item (first remaining-items)
+            id   (:org/id item)]
+        (when (checked-ids id)
+          (println "duplicate uuid found in file " file ".")
+          ;; (println "item: " item)
+          (println "id: " id)
+          (println "name: " (:org/name item)))
+        (when (seq remaining-items)
+          (recur (rest remaining-items) (conj checked-ids id)))))
+    (println (count items) " items checked in file " file)))
+
+(comment
+  (single-file-uuid-duplicate "/home/russ/todo/prompts.org"))
+
+(defn global-uuid-duplicates [fs]
+  (let [items (->> fs (mapcat core/path->flattened-items))]
+    (println "Checking for global uuids over " (count fs) " files and " (count items) " items.")
+    (loop [remaining-items items
+           checked-ids     #{}]
+      (let [item (first remaining-items)
+            id   (:org/id item)]
+        (when (checked-ids id)
+          (println "duplicate uuid found in file " (:org/source-file item) ".")
+          ;; (println "item: " item)
+          (println "id: " id)
+          (println "name: " (:org/name item)))
+        (when (seq remaining-items)
+          (recur (rest remaining-items) (conj checked-ids id)))))))
+
+(comment
+  (global-uuid-duplicates ["/home/russ/todo/prompts.org"]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; cli interface
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn print-report [& _args]
   (println "Running org-crud doctor!")
 
-  (let [files (org-files)]
+  (let [file-paths (org-files)]
+    ;; (println "Checking for duplicate uuids across " (count file-paths) " files.")
+    ;; (map single-file-uuid-duplicate file-paths)
+    (println "checking for global uuid duplicates")
+    (global-uuid-duplicates file-paths)
 
-    ;; duplicate ids?
-    (println "Checking for duplicate uuids across your files!")
-    ;; TODO impl as recursive walk
-    ;; may want to loop/recur, or create some api for traversing these trees
-    ;; NOTE ultimately we'll want to offer an api for updating at each leaf
-    ;; e.g. to fix the duplication problem for us by creating new uuids along the walk
-    (->> files
-         (map
-           (fn [f]
-             (let [ids (file->ids f)]
-               ids)))))
-  )
+    (println "Checking for broken org-links in your files")
+    (println "TODO: impl broken link fixer")
+
+    ))
+
 
 (comment
   (print-report))
