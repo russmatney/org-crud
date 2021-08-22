0a600c4c82470c4c814a14f6d5670ef82e26fa54
Russell Matney <russell.matney@gmail.com>
2021-08-22 16:41:29 -0400

    feat: port readme to org, org-roam dates-in-filename removal

--- src/user.clj
+++ src/user.clj
@@ -1,9 +1,15 @@
 (ns user
-  (:require [org-crud.update :as upd]
-            [org-crud.core :as org]
-            [wing.repl :as w.repl]
-            ))
+  (:require
+   [org-crud.update :as upd]
+   [org-crud.core :as org]
+   [wing.repl :as w.repl]
+   [clomacs :as clomacs]
+   [clojure.string :as string]
+   [babashka.fs :as fs]
+   [babashka.process :as process]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Ensuring uuids on all nodes
 
 (defn ensure-id [item]
   (let [id
@@ -15,13 +21,84 @@
         (assoc-in [:props :id] id))))
 
 (comment
-  (w.repl/sync-libs! :dev)
-
   (def --roam-dir "/home/russ/Dropbox/notes")
 
   (org/dir->nested-items --roam-dir)
   (upd/update-dir-with-fn! {:dir        --roam-dir
                             :recursive? true} ensure-id)
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; renaming org-roam files to move timestamps into property drawers
+
+(defn org->starts-with-datetime? [org]
+  ((comp (fn [fname]
+           (or
+             (string/starts-with? fname "2020")
+             (string/starts-with? fname "2021")))
+         fs/file-name :org/source-file)
+   org))
+
+(defn fname->data [s]
+  (let [groups
+        (->>
+          (re-seq #"(\d\d\d\d)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)-([\w_.]+)" s)
+          first
+          (drop 1))
+        new-fname                     (last groups)
+        [year month day hour min sec] (->> groups (take 6) (into []))]
+    {:new-filename new-fname
+     :created-at   (str year month day ":" hour min sec)}))
+
+(comment
+  (def --dir "/home/russ/Dropbox/todo/garden")
+
+  (def --ex-str "20201228124352-readme_idioms.org")
+
+  (fname->data --ex-str)
+
+  (->>
+    (org/dir->nested-items --dir)
+    (filter org->starts-with-datetime?)
+    ;; (map (comp fs/file-name :org/source-file))
+    (map (comp :org/source-file))
+    ;; (take 3)
+    count
+    )
+
+  (binding [upd/*item->source-file* :org/source-file]
+    (->>
+      (org/dir->nested-items --dir)
+      (filter org->starts-with-datetime?)
+      ;; (take 2)
+      (map (fn [org]
+             (let [
+                   {:keys [new-filename created-at]} ((comp fname->data fs/file-name :org/source-file) org)
+                   file-dir                          (-> org :org/source-file (#(string/replace % (fs/file-name %) "")))
+                   new-filepath                      (str file-dir new-filename)]
+               ;; (println "new-filename" new-filename)
+               ;; (println "filedir" file-dir)
+               ;; (println "new-filepath" new-filepath)
+
+               ;; update props
+               (upd/update! (:org/source-file org) org {:org.prop/created-at created-at})
+
+               ;; move file
+               (->
+                 ^{:out :string
+                   :dir file-dir}
+                 (process/$ mv ~(-> org :org/source-file) ~new-filepath)
+                 process/check
+                 :out))))))
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; misc
+
+(comment
+  (w.repl/sync-libs!)
 
 
   (defn missing-id [item]
@@ -35,5 +112,19 @@
         (println "missing id!" (:source-file item))
         (let [id (str (java.util.UUID/randomUUID))]
           {:id id :props {:id id}}))))
+
+
+
+  (clomacs/clomacs-defn
+    emacs-version
+    emacs-version)
+
+  (println (emacs-version))
+
+  (clomacs/clomacs-defn
+    clmx-current-org-item
+    clmx-current-org-item)
+
+  (println (clmx-current-org-item))
 
   )
